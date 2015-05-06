46ff094931167ebe035355fbf076011088cb77d4
Pedro Chinen <chinen93@gmail.com>
2015-05-06 02:14:55 -0300

    reiniciando projeto

--- lab08a_bigCalc/lab08a.c
+++ lab08a_bigCalc/lab08a.c
@@ -14,7 +14,7 @@ void printVetor(int *vetor, int tam){
   int i;
   printf("[");
   for(i=0; i<tam; i++){
-    printf("%d", *(vetor+i));
+    printf("%d;", *(vetor+i));
   }
   printf("]\n");
 }
@@ -38,64 +38,64 @@ void printResultado(int *vetor, int tam){
 
 /* realiza a adicao n1+n2, armazendo o resultado em result */
 void add(int *n1, int tam_n1, int *n2, int tam_n2, int *result) {
-  int i, j;
-  int pos1, pos2, posR, aux;
-  printVetor(n1, tam_n1);
-  printVetor(n2, tam_n2);
-  for(i=0; i<tam_n2; i++){
-    pos1 = tam_n1-i-1;
-    pos2 = tam_n2-i-1;
-    posR = tam_n1-i;
-
-    /*somar as duas parcelas*/
-    aux = *(n1+pos1) + *(n2+pos2);
-    if(aux >= 10){
-      *(result+posR) += aux%10;
-      *(result+posR-1) += 1;
-    }else
-      *(result+posR) += aux;
-  }/*for(i=0; i<tam_n2; i++)*/
-  for(i=0; i<tam_n1-tam_n2; i++){
-    pos1 = tam_n1-i-2;
-    *(result+posR) =  *(n1+pos1);
-  }
-  printVetor(result, tam_n1+1);
 }
 
 /* realiza a subtracao n1-n2, com n1>=n2, armazenando o resultado em result */
 void substract(int *n1, int tam_n1, int *n2, int tam_n2, int *result) {
   int i, j;
-  int pos1, pos2;
+  int pos1, pos2, posR;
+
+  printVetor(n1, tam_n1);
+  printVetor(n2, tam_n2);  
+  pos1=tam_n1-1;
+  pos2=tam_n2-1;
+  posR=tam_n1-1;
+  
+  /* subtrair as casas correspondentes */
   for(i=0; i<tam_n2; i++){
-    /* Subitrai unidade/unidade dezena/dezena e assim por diante */
-    pos1 = tam_n1-i-1;
-    pos2 = tam_n2-i-1;
-    /* Subtrai normalmente caso o valor de n2 seja menor que n1  */
+    /* se a posicao de n1 for menor que n2
+       pegar emprestado da posicao a esquerda de n1
+       se a posicao for 0 tranformar em 9
+       e continuar procurando um valor diferente de 0
+    */
     if(*(n1+pos1) < *(n2+pos2)){
-      /* Caso contrario, subtrair 1 no valor da esquerda de n1 
-         caso o valor seja 0, substitui-lo por 9 e ir mais a esquerda*/
-      for(j=1; j<=pos1; j++){
+      printf("{%d %d}\n", pos1, tam_n1-i-1);
+      /*TODO a segunda vez não funciona*/
+      for(j=i+1; j<tam_n1-i-1; j++){
 	if(*(n1+pos1-j) == 0)
-	  *(n1+pos1-j)=9;
-	else	  
-	  *(n1+pos1-j) -= 1;
+	  *(n1+pos1-j) = 9;
+	else{
+	  *(n1+pos1-j) =  *(n1+pos1-j)-1;
+	  break;
+	}
       }
-      *(n1+pos1) = *(n1+pos1)+pow(10, i+1);
-    }
+
+      *(n1+pos1) += 10;
+      printVetor(n2, tam_n2);
+      printVetor(n1, tam_n1);
+    }/*if(*(n1+pos1) < *(n2+pos2))*/
+
     /* efetua a subtracao */
-    *(result+pos1) =  *(n1+pos1) - *(n2+pos2);
+    *(result+posR) = *(n1+pos1) - *(n2+pos2); 
+
+    pos1--;
+    pos2--;
+    posR--;
   }/*for(i=0; i<tam_n2; i++)*/
-  /* Coloca as posicoes de n1, caso nao exista correspondente em n2  */
-  for(i=0;i<tam_n1-tam_n2;i++){
-    pos1 = tam_n1-i-2;
-    *(result+pos1) =  *(n1+pos1);
+
+  
+  /* colocar as outras casa de n1 no resultado */
+  for(i=0; i<tam_n1-tam_n2-1; i++){
+    *(result+posR) = *(n1+pos1);
+    pos1--;
+    posR--;
   }
+
+  printVetor(result, tam_n1);
 }
 
 /* realiza a multiplicacao n1*n2, armazendo o resultado em result */
 void multiply(int *n1, int tam_n1, int *n2, int tam_n2, int *result) {
-  printVetor(n1, tam_n1);
-  printVetor(n2, tam_n2);
 }
 
 int main() {
@@ -117,7 +117,7 @@ int main() {
     scanf(" %c", &aux);
     *(y+i)= aux-'0';
   }
-  
+  printf("--%c--\n\n",comando);
   switch(comando){
   case 'A':
     resultado=malloc((tamX+1) * sizeof(int));
@@ -130,18 +130,18 @@ int main() {
   case 'S':
     resultado=malloc(tamX * sizeof(int));
     for(i=0; i<tamX; i++){
-      *(resultado+i) = 0;
+      /* *(resultado+i) = 0; */
     }
     substract(x, tamX, y, tamY, resultado);
     printResultado(resultado, tamX);
     break;
   case 'M':
-    resultado=malloc(tamX * sizeof(int));
-    for(i=0; i<tamX; i++){
+    resultado=malloc((tamX+tamY) * sizeof(int));
+    for(i=0; i<(tamX+tamY); i++){
       *(resultado+i) = 0;
     }
     multiply(x, tamX, y, tamY, resultado);
-    printResultado(resultado, tamX);
+    printResultado(resultado, tamX+tamY);
     break;
   }
   
