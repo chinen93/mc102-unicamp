b19b0678056bfcc0ff63085c98a5912b37691ed3
Pedro Chinen <chinen93@gmail.com>
2015-05-07 16:23:52 -0300

    lab08b - possivel termino

--- lab08b_255TonsDeCinza/lab08b.c
+++ lab08b_255TonsDeCinza/lab08b.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
@@ -70,21 +71,102 @@ void escrever_pgm(int img[][MAX_TAM], int W, int H, int MaxV) {
 /* Transformacao de Potencia (gama) */
 void potencia(int img[][MAX_TAM], int W, int H, int MaxV, 
               double c, double gama) {
+    int x, y;
+    int aux;
+    for(y=0; y<H; y++){
+	for(x=0; x<W; x++){
+	    /* Percorre a imagem aplicando a funcao*/
+	    aux = (int) floor(c*pow(img[y][x], gama));
+	    if(aux > MaxV)
+		aux = MaxV;
+	    img[y][x] = aux;
+	}
+    }
     
 }
 
 /* Espelhamento */
 void espelhamento(int img[][MAX_TAM], int W, int H) {
-
+    int x, y;
+    for(y=0; y<H; y++){
+	for(x=0; x<W/2; x++){
+	    img[y][x] = img[y][(W-1)-x];
+	}
+    }
 }
 
 /* Reducao de Ruidos com Filtro Gaussiano */
 void reducaoRuido(int img[][MAX_TAM], int W, int H, int MaxV) {
+    int x, y;
+    int **aux;
+    int soma;
 
+    /* Alocar memoria */
+    aux =(int **)malloc(H * sizeof(int*));
+    for(y=0; y<H; y++)
+	aux[y] =(int *)malloc(W * sizeof(int));
+    
+    /* Andar pela matriz principal */
+    for(y=1; y<H-1; y++){
+	for(x=1; x<W-1; x++){
+	    /* Andar pelo filtro Gaussiano */
+	    soma = (1*img[y-1][x-1]+ 2*img[y-1][x]+ 1*img[y-1][x+1]+
+		    2*img[y][x-1]  + 4*img[y][x]  + 2*img[y][x+1]+
+		    1*img[y+1][x-1]+ 2*img[y+1][x]+ 1*img[y+1][x+1]
+		    )/16;
+	    if(soma > MaxV)
+		soma = MaxV;
+	    else
+		soma = floor(soma);
+	    aux[y][x]=soma;
+	}
+    }
+    /* Colocar as alteracoes na imagem */
+    for(y=1; y<H-1; y++){
+	for(x=1; x<W-1; x++){
+	    img[y][x]=aux[y][x];
+	}
+    }
+
+    /* Liberar memoria  */
+    for(y=0; y<H; y++){
+	free((int *)aux[y]);
+    }
+    free((int **)aux);
 }
 
 /* Rotacao 90 graus */
 void rotacao(int img[][MAX_TAM], int W, int H) {
+    int y, x;
+    int **aux;
+
+    /* Imagem original HxW 
+       Imagem auxiliar WxH
+     */
+    
+    /* Alocar Memoria */
+    aux=(int **)malloc(W * sizeof(int *));
+    for(y=0; y<W; y++)
+	aux[y]=(int *)malloc(H * sizeof(int));
+
+    /* Rotacionar para a aux */
+    /* aux[variavel][const] = img [const][variavel] */
+    for(y=0; y<W; y++){
+	for(x=0; x<H; x++){
+	    aux[x][y] = img[y][(W-1)-x];
+	}
+    }
+    /* Passar para a normal */
+    for(y=0; y<W; y++){
+	for(x=0; x<H; x++){
+	    img[y][x] = aux[y][x];
+	}
+    }
+    
+    /* Desalocar Memoria  */
+    for(y=0; y<H; y++)
+	free((int *)aux[y]);
+    free((int **)aux);
 
 }
 
