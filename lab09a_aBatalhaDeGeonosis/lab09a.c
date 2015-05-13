da195bcfefc144d88a2555d2ef973560b6e1ad05
Pedro Chinen <chinen93@gmail.com>
2015-05-13 20:48:11 -0300

    lab09a - erro compilacao, tentando novamente

--- lab09a_aBatalhaDeGeonosis/lab09a.c
+++ lab09a_aBatalhaDeGeonosis/lab09a.c
@@ -10,14 +10,31 @@
 #define FALSE 0
 #define MAX 50
 
-int dispHorizontal(int campo[MAX][MAX],
+int dispHorizontal(int *campo[MAX],
                    int i, int colunas);
-int dispVertical(int campo[MAX][MAX],
+int dispVertical(int *campo[MAX],
                  int j, int linhas);
-int dispDiagonal13(int campo[MAX][MAX],
+int dispDiagonal13(int *campo[MAX],
                     int i, int j, int linhas, int colunas);
-int dispDiagonal24(int campo[MAX][MAX],
+int dispDiagonal24(int *campo[MAX],
                     int i, int j, int linhas, int colunas);
+
+
+void printVetor(int *vetor, int tam){
+    int i;
+    for(i=0; i<tam; i++)
+        printf("[%d]", vetor[i]);
+    printf("\n");
+}
+void printMatriz(int *matriz[MAX], int linhas, int colunas){
+    int i, j;
+    for(i=0; i<linhas; i++){
+        for(j=0; j<colunas; j++)
+            printf("{%d}",matriz[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main(){
     int linhas, colunas;
@@ -32,7 +49,7 @@ int main(){
     scanf("%d %d", &linhas, &colunas);
     campo=(int **)malloc(linhas * sizeof(int *));
     for(i=0; i<linhas; i++)
-        campo[i] = (int)malloc(colunas * sizeof(int));
+        campo[i] = (int *)malloc(colunas * sizeof(int));
     for(i=0; i<linhas; i++)
         for(j=0; j<colunas; j++)
             scanf(" %d", &campo[i][j]);
@@ -44,68 +61,92 @@ int main(){
 
     /* Fazer as contas antes de posicionar o AT-TE */
     for(i=0; i<linhas; i++)
-        horizontal[i] = dispHorizontal(campo[MAX][MAX],
-                                       i, linhas, colunas);    
+        horizontal[i] = dispHorizontal(campo,
+                                       i, colunas);    
     for(i=0; i<colunas; i++)
-        vertical[i] = dispVertical(campo[MAX][MAX],
-                                   i, linhas, colunas);
+        vertical[i] = dispVertical(campo,
+                                   i, linhas);
   
     for(j=0; j<colunas; j++)
-        diagonal13[i] = dispDiagonal13(campo[MAX][MAX],
-                                       0, i, linhas, colunas);
+        diagonal13[j] = dispDiagonal13(campo,
+                                       0, j, linhas, colunas);
     for(i=1; i<linhas; i++)
-        diagonal13[colunas+i] = dispDiagonal13(campo[MAX][MAX],
+        diagonal13[colunas-1+i] = dispDiagonal13(campo,
                                        i, colunas-1, linhas, colunas);
     
     for(i=0; i<linhas; i++)
-        diagonal24[i] = dispDiagonal13(campo[MAX][MAX],
+        diagonal24[i] = dispDiagonal24(campo,
                                        i, colunas-1, linhas, colunas);
     for(j=1; j<colunas; j++)
-        diagonal24[linhas+j] = dispDiagonal13(campo[MAX][MAX],
+        diagonal24[linhas-1+j] = dispDiagonal24(campo,
                                               linhas-1, colunas-1-j,
                                               linhas, colunas);
+
+    printf("\n");
+    printVetor(diagonal13, linhas+colunas-1);
+    printVetor(diagonal24, linhas+colunas-1);
+    printf("\n");
+    printMatriz(campo, linhas, colunas);
+    
+    
     /* Posicionar o AT-TE  */
     for(i=0; i<linhas; i++)
         for(j=0; j<colunas; j++){
-            droids=0;
-            disparos=0;
-            if(!horizontal[i]){
-                disparos++;
-                droids+=horizontal[i];
-            }
+            if(!campo[i][j]){
+                droids=0;
+                disparos=0;
+                if(horizontal[i]){
+                    printf("hor = %d\n",i);
+                    disparos++;
+                    droids+=horizontal[i];
+                }
 
-            if(!vertical[j]){
-                disparos++;
-                droids+=vertical[j];
-            }
+                if(vertical[j]){
+                    printf("ver = %d\n",j);
+                    disparos++;
+                    droids+=vertical[j];
+                }
 
-            if(!diagonal13[i+j]){
-                disparos++;
-                droids+=diagonal13[i+j];
-            }
+                if(diagonal13[i+j]){
+                    printf("d13 = %d\n",i+j);
+                    disparos++;
+                    droids+=diagonal13[i+j];
+                }
 
-            if(!diagonal24[i-j+colunas-1]){
-                disparos++;
-                droids+=diagonal24[i-j+colunas-1];
-            }
+                if(diagonal24[i-j+colunas-1]){
+                    printf("d24 = %d\n",i-j+colunas-1);
+                    disparos++;
+                    droids+=diagonal24[i-j+colunas-1];
+                }
 
-            if(droids > melhor[2]){
-                melhor[0] = i;
-                melhor[1] = j;
-                melhor[2] = droids;
-                melhor[3] = disparos;
-            }
-            else if(melhor[2] == droids){
-                if(i <melhor[0]){
+                if(droids > melhor[2]){
                     melhor[0] = i;
                     melhor[1] = j;
                     melhor[2] = droids;
                     melhor[3] = disparos;
                 }
-                else if(j < melhor[1])
-            }
+                else if(melhor[2] == droids){
+                    if(i <melhor[0]){
+                        melhor[0] = i;
+                        melhor[1] = j;
+                        melhor[2] = droids;
+                        melhor[3] = disparos;
+                    }
+                    else if(i == melhor[0] && j < melhor[1]){
+                        melhor[1] = j;
+                        melhor[2] = droids;
+                        melhor[3] = disparos;
+                    }
+                }
+                printVetor(melhor, 4);
+            }/*if(!campo[i][j])*/
         }/*for(j=0; j<colunas; j++)*/
 
+
+    printf("Posicao = (%d,%d)\n", melhor[0], melhor[1]);
+    printf("Droids = %d\n",melhor[2]);
+    printf("Disparos = %d\n", melhor[3]);
+    
     /* Desalocar Memoria */    
     for(i=0; i<linhas; i++)
         free(campo[i]);
@@ -118,21 +159,21 @@ int main(){
 }
 
 
-int dispHorizontal(int campo[MAX][MAX],
+int dispHorizontal(int *campo[MAX],
                    int i, int colunas){
     int k, droids=0;
     for(k=0; k<colunas; k++)
         droids+=campo[i][k];
     return droids;
 }
-int dispVertical(int campo[MAX][MAX],
+int dispVertical(int *campo[MAX],
                  int j, int linhas){
     int k, droids=0;
     for(k=0; k<linhas; k++)
         droids+=campo[k][j];
     return droids;
 }
-int dispDiagonal13(int campo[MAX][MAX],
+int dispDiagonal13(int *campo[MAX],
                    int i, int j, int linhas, int colunas){
     int k, l, droids=0;
     k=i+j;
@@ -148,7 +189,7 @@ int dispDiagonal13(int campo[MAX][MAX],
     }
     return droids;
 }
-int dispDiagonal24(int campo[MAX][MAX],
+int dispDiagonal24(int *campo[MAX],
                    int i, int j, int linhas, int colunas){
     int k, l, droids=0;
     k=0;
@@ -162,4 +203,5 @@ int dispDiagonal24(int campo[MAX][MAX],
         k++;
         l++;
     }
+    return droids;
 }
---
+++ lab09b_peladeiros/lab09b.c
@@ -0,0 +1,17 @@
+/* Nome: Pedro Hideaki Uiechi Chinen
+   RA  : 175828
+   Lab 09b - Peladeiros */
+
+
+/*dado um grupo de n jogadores confirmados, os t capitães são definidos com base nas t maiores notas médias atribuídas para cada jogador. 
+Definidos os capitães, inicia-se o processo de escolha alternada entre eles, de tal modo que o primeiro capitão a escolher é o que possui maior nota média, o segundo é o que possui a segunda maior nota média e assim sucessivamente. 
+Tal como dito anteriormente, cada capitão sempre escolhe um jogador, dentre os jogadores ainda não escolhidos, que ele acredita ser o melhor.
+Nesse caso, um capitão C considera um jogador A melhor que um jogador B se a nota atribuída pelo capitão C ao jogador A é maior do que a nota atribuída pelo capitão C ao jogador B.
+Casos de empate, tanto na definição dos capitães quanto na escolha dos jogadores pelos capitães, devem ser resolvidos em favor do jogador com o menor número identificador, isto é, caso os jogadores A e B tenham notas iguais, considera-se que jogador A é melhor que o jogador B se A < B. 
+
+
+*/
+
+int main(){
+    return 0;
+}
